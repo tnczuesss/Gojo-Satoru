d89ef1b4f42aaca4b670484e189e06ecc491fe8d
tnczuesss <112687094+tnczuesss@users.noreply.github.com>
2022-09-02 22:35:31 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,18 +19,18 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['Your Number']
-global.premium = ['Your Number']
-global.ownernomer = 'Your Number'
-global.ownername = '🐦Star Boy Nexus'
-global.botname = 'Gojo-Satoru'
-global.footer = '©NexusNwInc.'
+global.owner = ['94701358330']
+global.premium = ['94701358330']
+global.ownernomer = '94701358330'
+global.ownername = '🍁අතිගරු සයිබර් සෝල්🍁'
+global.botname = '🍁අතිගරු සයිබර් සෝල් උතුමානන්🍁'
+global.footer = '🍁අතිගරු සයිබර් සෝල්❤️.'
 global.ig = 'https://github.com/nexusNw'
 global.region = 'India, South India, Kerala'
 global.sc = 'https://github.com/nexusNw/Gojo-Satoru'
-global.myweb = 'https://youtube.com/channel/UCqoUjPvDdb0kjXNYdvPPpHQ'
-global.packname = 'Gojo-Satoru'
-global.author = '🐦Star Boy Nexus'
+global.myweb = 'https://www.tiktok.com/@tncbatta?_t=8VLrLhYjTIP&_r=1'
+global.packname = '🍁අතිගරු සයිබර් සෝල්❤️'
+global.author = '🐦tnczues'
 global.sessionName = 'session'
 global.prefa = ['','!','.','🐦','🐤','🗿']
 global.sp = '⭔'
